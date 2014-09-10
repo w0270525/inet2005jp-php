644d6cb0fe3d4ee1dda1d24bd302b92eee0f0daa
Jonathan Prince <w0270525@nscc.ca>
2014-09-10 16:19:12 -0700

    second commit

---
+++ PHPtest/index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<!DOCTYPE html>”;
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Today's Date Page</title>";
+    echo "</head>";
+    echo "<body>";
+    echo "<h1>Today is ";
+    echo date("Y/m/d");
+    echo ".</h1>";
+    echo "</body>";
+    echo "</html>";
+?>
