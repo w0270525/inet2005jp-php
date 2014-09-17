77c4ecb5224ee468f66038badaddeebde7752c53
Jonathan Prince <w0270525@nscc.ca>
2014-09-16 20:39:01 -0700

    kilopound worked out, starting part b

---
+++ Lab1/partb.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<?php
+	echo "Greetings from Lab 1.";
+?>
+<p>
+	<?php echo "<h3>Hi. This is an 'H3' tag.</h3>";	?>
+
+</p>
+
+</body>
+</html>
