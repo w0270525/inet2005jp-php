246eb617b18b29187520c1445b780a1656285a86
Jonathan Prince <w0270525@nscc.ca>
2014-09-18 06:04:55 -0700

    Some progress on Lab 2.

--- Lab2/index.php
+++ Lab2/index.php
@@ -6,6 +6,42 @@
 </head>
 <body>
 <?php
+function thatCrazyFunction($inputtedString) {
+
+	switch($inputtedString){
+		case "1":
+			echo "<h1>This is an H1</h1>";
+			break;
+		case "2":
+			echo "<h2>This is an H2</h2>";
+			break;
+		case "3":
+			echo "<h3>This is an H3</h3>";
+			break;
+		case "4":
+			echo "<h4>This is an H4</h4>";
+			break;
+		case "5":
+			echo "<h5>This is an H5</h5>";
+			break;
+		case "6":
+			echo "<h6>This is an H6</h6>";
+			break;
+		default:
+			echo "Error. please choose 1-6";
+
+	}//End switch
+}//End function
+
+
+//needs to be made in a loop.
+thatCrazyFunction("1");
+thatCrazyFunction("2");
+thatCrazyFunction("3");
+thatCrazyFunction("4");
+thatCrazyFunction("5");
+thatCrazyFunction("6");
+thatCrazyFunction("7");
 
 
 ?>
