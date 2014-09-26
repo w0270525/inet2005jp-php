624266501ac880a977beaea534320b4e92394db2
Jonathan Prince <w0270525@nscc.ca>
2014-09-26 10:39:26 -0700

    Looped messages of fahrenheit to celsius working.

--- Lab2/FahrenheitConversion.php
+++ Lab2/FahrenheitConversion.php
@@ -8,12 +8,15 @@
 <table>
 <?php
 
-for ($i =0; $i <=100; $i++) {
+$celsius;
+for ($fahrenheit =0; $fahrenheit <=100; $fahrenheit++) {
+	$celsius = ($fahrenheit - 32) * (5/9);
+	$celsius = round($celsius,0);?>
 
-}
-
-
-?>
+<p>
+<?php echo $fahrenheit; ?> degree(s) fahrenheit equals <?php echo $celsius; ?> degrees Celsius.
+	</p>
+<?php } ?>
 </table>
 </body>
 </html>
