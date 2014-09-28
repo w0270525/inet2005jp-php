548e896d3094d5637e979b81757cda77672050b1
Jonathan Prince <w0270525@nscc.ca>
2014-09-28 15:02:46 -0700

    Initial commit of films.php

--- Lab3/films.php
+++ Lab3/films.php
@@ -7,7 +7,16 @@
 	</title>
 </head>
 <body>
+<!--
+<?php
+$db = mysqli_connect("localhost","user", "inet2005","sakila");
+if (!$db)
+{
+die('Could not connect to the Sakila Database: ' . mysqli_error($db));
+}
 
-<p></p>
+?>
+-->
+<p>Hi</p>
 </body>
 </html>
