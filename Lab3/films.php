3095e5bbed0793dfa053f1ef117ad69e77e46038
Jonathan Prince <w0270525@nscc.ca>
2014-09-28 16:27:11 -0700

    films outputting not in a table.

--- Lab3/films.php
+++ Lab3/films.php
@@ -7,16 +7,41 @@
 	</title>
 </head>
 <body>
-<!--
+<table border="1px">
 <?php
-$db = mysqli_connect("localhost","user", "inet2005","sakila");
+$db = mysqli_connect("localhost","inet2005", "itCampus2014","sakila");
 if (!$db)
 {
 die('Could not connect to the Sakila Database: ' . mysqli_error($db));
 }
 
+$result = mysqli_query($db,"SELECT * FROM film LIMIT 0,10");
+
+if(!$result)
+{
+	die('Could not retrieve records from the Sakila Database: ' . mysqli_error($db));
+}
+while ($row = mysqli_fetch_assoc($result))
+{
+	echo $row['title'] . " " . $row['description'];
+	echo "<br />";
+}
+
+$result = mysqli_query($db,"SELECT * FROM film LIMIT 0,10");
+if(!$result)
+{
+	die('Could not retrieve records from the Sakila Database: ' . mysqli_error($db));
+}
+while ($row = mysqli_fetch_assoc($result))
+{
+	echo $row['title'] . " " . $row['description'];
+	echo "<br />";
+}
+mysqli_close($db);
+
+
 ?>
--->
-<p>Hi</p>
+</table>
+
 </body>
 </html>
