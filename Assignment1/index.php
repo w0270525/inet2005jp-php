5fce5bb8b72db537f66dec6f677d405b75c9a940
Jonathan Prince <w0270525@nscc.ca>
2014-10-05 17:12:50 -0700

    some more work on gettting the table put in and started the form. not complete

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -14,10 +14,12 @@ $db = connectToDB();
 ?>
 <?php
  ?>
+<h1>Search First and Last Names from Database</h1>
+<form id="employee_query" method="post"  onsubmit="" </form>
 
 <table border="1px">
 	<tr>
-		<th>First Name</th><th>Last Name</th><th>Last Updated</th>
+		<th>Emp. Number</th><th>Birth Date</th><th>First Name</th><th>Last Name</th><th>Gender</th><th>Hire Date</th>
 	</tr>
 	<?php
 
@@ -27,10 +29,19 @@ $db = connectToDB();
 
 	<tr>
 		<td>
-			<?php echo $row['actor_id']; ?><?php echo $row['first_name']; ?> </td><td> <?php echo $row['last_name']; ?></td><td> <?php echo $row['last_update']; ?></td>
+			<?php echo $row['emp_no']; ?></td><td><?php echo $row['birth_date']; ?> </td><td> <?php echo $row['first_name']; ?></td><td> <?php echo $row['last_name']; ?></td><td><?php echo $row['gender']; ?></td><td><?php echo $row['hire_date']; ?></td>
 	</tr>
-<?php
-}//end while.
-?>
+
+	<?php }//end while. ?>
+
+
+
+
+
+	<?php
+	//to close database connection.
+	mysqli_close($db);
+	?>
+
 </body>
 </html> 
