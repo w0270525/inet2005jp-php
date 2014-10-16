fff7bff2f8a996448255ce49bd0e01267056c158
Jonathan Prince <w0270525@nscc.ca>
2014-10-15 21:38:49 -0700

    oh god!

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -26,6 +26,10 @@ if(isset($_POST['query']))
 if(isset($_SESSION['currentnumber']))
 {
 	$currentnumber = $_SESSION['currentnumber'];
+	if(isset($_SESSION['next']))
+		$currentnumber+=25;
+	elseif (isset($_SESSION['prev']) && $currentnumber>0)
+		$currentnumber-=25;
 }
 else {
 	$currentnumber=0;
@@ -33,17 +37,47 @@ else {
 }
 
 
+if(isset($_SESSION['query'])){
+$querytotal==mysqli_query($db, 'SELECT * FROM employees.employees where (last_name LIKE "%'.$query.'%") or (first_name like "%'.$query.'%")');
+while ($count= mysqli_fetch_assoc($querytotal)) {
+	$count+=1;
+	$_SESSION['count']=$count;
+}
+
 $queryresult=mysqli_query($db, 'SELECT * FROM employees.employees where (last_name LIKE "%'.$query.'%") or (first_name like "%'.$query.'%") order by first_name LIMIT '.$currentnumber.',25; ');
+}
 ?>
 
 
 <a href='logout.php'>logout</a>
+
 <h1>Search First and Last Names from Database</h1>
+
 <form id="employee_query" method="post"  onsubmit="index.php" >
 	<input name="emp_query" type="text">
 
 
 	<input type="submit" name="query" id="query" value="Employee Query">
+
+</form>
+
+
+<form id="nav" method="post"  onsubmit="index.php" >
+		<?php if($currentnumber!=0){
+
+			echo  '<input type="submit" name="prev" id="prev" value="Prev">';
+		}
+		?>
+
+<?php if ($count>= $currentnumber+25)
+echo '<input type="submit" name="next" id="next" value="Next">';
+?>
+
+</form>
+
+<form id="addemployee" method="post" onsubmit="addemployee.php">
+
+	<input type="submit" name="add" id="add" value="Add an Employee">
 
 </form>
 
@@ -71,14 +105,16 @@ $queryresult=mysqli_query($db, 'SELECT * FROM employees.employees where (last_na
 			<td><?php echo $row['last_name']; ?></td>
 			<td><?php echo $row['gender']; ?></td>
 			<td><?php echo $row['hire_date']; ?></td>
-			<td><?php echo '<a href="edit.php"></a>' ?></td>
-			<td><?php echo '<a href="delete.php"></a>' ?></td>
+			<td><?php echo '<div class="edit"><img src="pencil.png"></div>' ?></td>
+			<td><?php echo '<div class="delete"><img src="delete.png"></div>' ?></td>
 		</tr>
 
 	<?php }//end while. ?>
 
 
 </table>
+
+
 <?php
 if ($currentnumber)
 
