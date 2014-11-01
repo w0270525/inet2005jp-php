6081439bf6d53950508c0a4da962960216151de4
Jonathan Prince <w0270525@nscc.ca>
2014-11-01 15:11:57 -0700

    Getting ready to complete the lab 4.

--- Lab4/index.php
+++ Lab4/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <title>Shape Area Calculator</title>
 </head>
 <body>
 <?php
-
+require_once("Shape.php");
+require_once("Rectangle.php");
+require_once("Circle.php");
+require_once("Triangle.php");
 
 
 ?>
