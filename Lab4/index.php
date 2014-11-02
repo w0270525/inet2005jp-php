36d3013a3458dc45647ca4d21d656a04041519f7
Jonathan Prince <w0270525@nscc.ca>
2014-11-02 08:14:43 -0800

    working on the form on index. thrown together quickly in dreamweaver

--- Lab4/index.php
+++ Lab4/index.php
@@ -12,6 +12,45 @@ require_once("Triangle.php");
 ?>
 
 
+<form id="form1" name="form1" method="post" action="">
+	<fieldset>
+		<legend>Circle</legend>
+		<p>
+			<label>Radius
+				<input type="text" name="Radius" id="Radius" />
+			</label>
+		</p>
+	</fieldset>
+	<fieldset>
+		<legend>Rectangle</legend>
+		<p>
+			<label>Length
+				<input type="text" name="rectLength" id="rectLength" />
+			</label>
+		</p>
+		<p>
+			<label>Width
+				<input type="text" name="rectWidth" id="rectWidth" />
+			</label>
+		</p>
+	</fieldset>
+	<fieldset><legend>Triangle</legend>
+		<p>
+			<label>Base
+				<input type="text" name="triBase" id="triBase" />
+			</label>
+		</p>
+		<p>
+			<label>Height
+				<input type="text" name="triHeight" id="triHeight" />
+			</label>
+		</p>
+	</fieldset>
+	<input name="Calculate" type="submit" value="Calculate" />
+</form>
+<p>Results:</p>
+<p></p>
+
 
 
 </body>
