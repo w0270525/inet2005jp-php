627ef0c39793870b7cbd6de7fcfa1a17c81b2d1a
Jonathan Prince <w0270525@nscc.ca>
2014-12-15 11:36:51 -0800

    Lab 8 wasn't committing properly, had to create a new folder. hopefully this looks ok.

---
+++ Lab 8/module/Album/Module.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 12/1/14
+ * Time: 10:02 AM
+ */
+
+namespace Album;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+	public function getAutoloaderConfig()
+	{
+		return array(
+			'Zend\Loader\ClassMapAutoloader' => array(
+				__DIR__ . '/autoload_classmap.php',
+			),
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
+
+	public function getConfig()
+	{
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+	public function getServiceConfig()
+	{
+		return array(
+			'factories' => array(
+				'Album\Model\AlbumTable' =>  function($sm) {
+						$tableGateway = $sm->get('AlbumTableGateway');
+						$table = new AlbumTable($tableGateway);
+						return $table;
+					},
+				'AlbumTableGateway' => function ($sm) {
+						$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+						$resultSetPrototype = new ResultSet();
+						$resultSetPrototype->setArrayObjectPrototype(new Album());
+						return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+					},
+			),
+		);
+	}
+
+}
